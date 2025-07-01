720e5b8dd4894d6ff4fd6a9a37a513cae5793d4e
Stephen Szpak <srszpak@gmail.com>
2025-07-01 10:07:22 -0500

    Start custom supervisors

--- mmo_server/lib/mmo_server/application.ex
+++ mmo_server/lib/mmo_server/application.ex
@@ -11,8 +11,8 @@ defmodule MmoServer.Application do
       MmoServerWeb.Presence,
       MmoServer.Protocol.UdpServer,
       {Horde.Registry, [name: PlayerRegistry, keys: :unique]},
-      {Horde.DynamicSupervisor, [name: PlayerSupervisor, strategy: :one_for_one]},
-      {Horde.DynamicSupervisor, [name: ZoneSupervisor, strategy: :one_for_one]}
+      {MmoServer.PlayerSupervisor, []},
+      {MmoServer.ZoneSupervisor, []}
     ]
 
     opts = [strategy: :one_for_one, name: MmoServer.Supervisor]

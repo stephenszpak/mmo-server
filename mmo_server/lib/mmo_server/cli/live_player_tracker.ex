2b83f29b00b78e57c8292681a0de9cd30306bbe5
Stephen Szpak <srszpak@gmail.com>
2025-07-01 09:24:22 -0500

    Add live player tracking CLI and dashboard

---
+++ mmo_server/lib/mmo_server/cli/live_player_tracker.ex
@@ -0,0 +1,19 @@
+defmodule MmoServer.CLI.LivePlayerTracker do
+  @moduledoc """
+  Helper utilities for inspecting live player processes.
+  """
+
+  @doc """
+  Queries all currently active players and prints their positions.
+  """
+  def print_all_positions do
+    players = Horde.Registry.select(PlayerRegistry, [{{{:"$1", :_, :_}, [], ["$1"]}}])
+
+    IO.puts("player_id | x | y | z")
+
+    Enum.each(players, fn id ->
+      {x, y, z} = GenServer.call({:via, Horde.Registry, {PlayerRegistry, id}}, :get_position)
+      IO.puts("#{id} | #{x} | #{y} | #{z}")
+    end)
+  end
+end

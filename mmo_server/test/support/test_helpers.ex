5954241f97986ec69ee208fd8b12234ddf18bc6e
Stephen Szpak <srszpak@gmail.com>
2025-07-03 08:14:43 -0500

    fix test helper

--- mmo_server/test/support/test_helpers.ex
+++ mmo_server/test/support/test_helpers.ex
@@ -1,4 +1,5 @@
 defmodule MmoServer.TestHelpers do
+  import ExUnit.Callbacks, only: [start_supervised: 1]
   def start_shared(process_mod, args \\ []) do
     {:ok, pid} = start_supervised({process_mod, args})
     Ecto.Adapters.SQL.Sandbox.allow(MmoServer.Repo, self(), pid)

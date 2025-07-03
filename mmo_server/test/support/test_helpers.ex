321dc5820baceb76e9de6f89624ac45ab163db7d
Stephen Szpak <srszpak@gmail.com>
2025-07-03 15:04:12 -0500

    Apply allow_tree for sandbox in start_shared

--- mmo_server/test/support/test_helpers.ex
+++ mmo_server/test/support/test_helpers.ex
@@ -4,7 +4,7 @@ defmodule MmoServer.TestHelpers do
   def start_shared(process_mod, args \\ []) do
     child_spec = Supervisor.child_spec({process_mod, args}, id: {process_mod, make_ref()})
     {:ok, pid} = start_supervised(child_spec)
-    Ecto.Adapters.SQL.Sandbox.allow(MmoServer.Repo, self(), pid)
+    allow_tree(MmoServer.Repo, self(), pid)
     pid
   end
 

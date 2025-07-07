cbc4346ec64f9e824c5fe767497f53f0ea79655e
Stephen Szpak <srszpak@gmail.com>
2025-07-07 09:34:56 -0500

    Add CSRF token handling for LiveView

--- mmo_server/priv/static/assets/app.js
+++ mmo_server/priv/static/assets/app.js
@@ -2,7 +2,12 @@
 // objects on the global `window`. These provide the `Socket` and
 // `LiveSocket` constructors. Since the static assets are served directly
 // without a bundler, we use the globals instead of ES module imports.
-let liveSocket = new LiveView.LiveSocket("/live", Phoenix.Socket)
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content")
+let liveSocket = new LiveView.LiveSocket("/live", Phoenix.Socket, {
+  params: {_csrf_token: csrfToken}
+})
 liveSocket.connect()
 
 window.liveSocket = liveSocket

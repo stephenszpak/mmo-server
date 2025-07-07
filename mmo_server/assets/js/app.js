ec76d0ed7a19e0f3145a5a91bf89bab8d9785afc
Stephen Szpak <stephenszpak@Stephens-MacBook-Air.local>
2025-07-07 08:55:02 -0500

    removed unnecessary asset folder

--- mmo_server/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-import {Socket} from "/js/phoenix.min.js"
-import {LiveSocket} from "/js/phoenix_live_view.min.js"
-
-let liveSocket = new LiveSocket("/live", Socket)
-liveSocket.connect()
-
-window.liveSocket = liveSocket

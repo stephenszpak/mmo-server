f252798856112214c8484812cf71fe1f03889680
Stephen Szpak <srszpak@gmail.com>
2025-07-08 11:00:46 -0500

    Consolidate seed scripts

--- mmo_server/lib/mix/tasks/seed.ex
+++ mmo_server/lib/mix/tasks/seed.ex
@@ -1,52 +1,9 @@
 defmodule Mix.Tasks.Seed do
   use Mix.Task
-  alias MmoServer.{Repo, PlayerPersistence, PlayerStats, LootDrop}
 
-  @shortdoc "Seed test zones and players"
+  @shortdoc "Seed the database, zones and players"
   def run(_args) do
     Mix.Task.run("app.start")
-
-    for zone <- ["elwynn", "durotar"] do
-      :ok = MmoServer.ZoneManager.ensure_zone_started(zone)
-    end
-
-    for player <- ["thrall", "jaina", "arthas"] do
-      zone = Enum.random(["elwynn", "durotar"])
-
-      attrs = %{
-        id: player,
-        zone_id: zone,
-        x: :rand.uniform() * 100,
-        y: :rand.uniform() * 100,
-        z: 0.0,
-        hp: 100,
-        status: "alive"
-      }
-
-      %PlayerPersistence{}
-      |> PlayerPersistence.changeset(attrs)
-      |> Repo.insert!(on_conflict: :replace_all, conflict_target: :id)
-
-      %PlayerStats{}
-      |> PlayerStats.changeset(%{player_id: player, xp: 0, level: 1, next_level_xp: 100})
-      |> Repo.insert!(on_conflict: :replace_all, conflict_target: :player_id)
-
-      Horde.DynamicSupervisor.start_child(
-        MmoServer.PlayerSupervisor,
-        MmoServer.Player.child_spec(%{player_id: player, zone_id: zone})
-      )
-    end
-
-    loot_drops = [
-      %{npc_id: "wolf_seed1", item: "wolf_pelt", zone_id: "durotar", x: 10.0, y: 5.0, z: 0.0, quality: "common", picked_up: false},
-      %{npc_id: "wolf_seed2", item: "wolf_pelt", zone_id: "durotar", x: 12.0, y: 8.0, z: 0.0, quality: "common", picked_up: false},
-      %{npc_id: "wolf_seed3", item: "wolf_pelt", zone_id: "elywnn", x: 20.0, y: 15.0, z: 0.0, quality: "common", picked_up: false}
-    ]
-
-    for attrs <- loot_drops do
-      %LootDrop{}
-      |> LootDrop.changeset(attrs)
-      |> Repo.insert!(on_conflict: :replace_all, conflict_target: :id)
-    end
+    MmoServer.Seeds.run()
   end
 end
--- mmo_server/priv/repo/seeds.exs
+++ mmo_server/priv/repo/seeds.exs
@@ -1,25 +1,2 @@
-alias MmoServer.{Repo, MobTemplate}
-alias MmoServer.{Quest}
-
-for template <- [
-  %{id: "wolf", name: "Wolf", hp: 30, damage: 15, xp_reward: 20, aggressive: true, loot_table: [
-      %{item: "wolf_pelt", chance: 1.0, quality: "common"}
-    ]},
-  %{id: "goblin_scout", name: "Goblin Scout", hp: 20, damage: 10, xp_reward: 10, aggressive: false, loot_table: []},
-  %{id: "dungeon_boss", name: "Dungeon Boss", hp: 100, damage: 25, xp_reward: 250, aggressive: true, loot_table: [
-      %{item: "legendary_sword", chance: 0.05, quality: "rare"}
-    ]}
-] do
-  %MobTemplate{}
-  |> MobTemplate.changeset(template)
-  |> Repo.insert!(on_conflict: :replace_all, conflict_target: :id)
-end
-
-for quest <- [
-  %{id: MmoServer.Quests.wolf_kill_id(), title: "Cull the Wolves", description: "Kill 3 wolves", objectives: [ %{type: "kill", target: "wolf", count: 3} ], rewards: [ %{ "type" => "xp", "amount" => 100} ]},
-  %{id: MmoServer.Quests.pelt_collect_id(), title: "Gather Pelts", description: "Collect 2 wolf pelts", objectives: [ %{type: "collect", target: "wolf_pelt", count: 2} ], rewards: [ %{ "item" => "wolf_cape"} ]}
-] do
-  %Quest{}
-  |> Quest.changeset(quest)
-  |> Repo.insert!(on_conflict: :replace_all, conflict_target: :id)
-end
+Mix.Task.run("app.start")
+MmoServer.Seeds.run()

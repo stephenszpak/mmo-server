dfe748bb1e0cfe7cf94926dc5c7a723f13142415
Stephen Szpak <srszpak@gmail.com>
2025-07-08 08:45:53 -0500

    Reduce LiveView log noise

--- mmo_server/config/dev.exs
+++ mmo_server/config/dev.exs
@@ -21,4 +21,6 @@ config :mmo_server, MmoServerWeb.Endpoint,
 config :mmo_server, MmoServerWeb.Endpoint,
   live_reload: [patterns: [~r"priv/static/.*", ~r"priv/gettext/.*", ~r"lib/mmo_server_web/(controllers|live|components)/.*"]]
 
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console,
+  format: "[$level] $message\n",
+  level: :info
